2871a8dfdb08ecb97f0440125c37ff05d0f10f7e
Fabo xw <fabianmartinezrincon.123@gmail.com>
2022-12-27 19:06:44 -0300

    Agregando el id

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,14 @@ import { useState } from 'react';
 import { createClient } from '@supabase/supabase-js';
 
 
+const supabaseUrl = 'https://katkzhgmwoqfjrdvgtqr.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImthdGt6aGdtd29xZmpyZHZndHFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzIxNjg2OTgsImV4cCI6MTk4Nzc0NDY5OH0.ymjFIHRU9wi9HzaYbQH22ESIJz_S4lGyuHr_DjPLU4g'
+const supabaseAdmin = createClient(supabaseUrl, supabaseKey)
 
-const supabaseAdmin = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL || '',
-  process.env.SUPABASE_SERVICE_ROLE_KEY || ''
-);
+//const supabaseAdmin = createClient(
+//  process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+//  process.env.SUPABASE_SERVICE_ROLE_KEY || ''
+//);
 
 export async function getStaticProps() {
   //await supabaseAdmin.from('images').insert([{
