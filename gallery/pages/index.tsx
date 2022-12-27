104bca0dd0b0bc77dcc3b79169696fe17f533185
Fabo xw <fabianmartinezrincon.123@gmail.com>
2022-12-27 18:34:52 -0300

    Sacando los codigos para modificar la base de datos

--- gallery/pages/index.tsx
+++ gallery/pages/index.tsx
@@ -10,20 +10,20 @@ const supabaseAdmin = createClient(
 );
 
 export async function getStaticProps() {
-  await supabaseAdmin.from('images').insert([{
-    name: 'Pedro Duarte',
-    href: 'https://twitter.com/peduarte/status/1463897468383412231',
-    username: '@peduarte',
-    imageSrc: 'https://katkzhgmwoqfjrdvgtqr.supabase.co/storage/v1/object/sign/images-links/1.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJpbWFnZXMtbGlua3MvMS5wbmciLCJ0cmFuc2Zvcm1hdGlvbnMiOiIiLCJpYXQiOjE2NzIxNzYwNTUsImV4cCI6MTk4NzUzNjA1NX0.Kl9B9-RhPtW-32kgU61Gkqq4RxPHc3k8b8PFtQXHjj0',
-  }]);
-
+  //await supabaseAdmin.from('images').insert([{
+  //  name: 'Pedro Duarte',
+  //  href: 'https://twitter.com/peduarte/status/1463897468383412231',
+  //  username: '@peduarte',
+  //  imageSrc: 'https://katkzhgmwoqfjrdvgtqr.supabase.co/storage/v1/object/sign/images-links/1.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJpbWFnZXMtbGlua3MvMS5wbmciLCJ0cmFuc2Zvcm1hdGlvbnMiOiIiLCJpYXQiOjE2NzIxNzYwNTUsImV4cCI6MTk4NzUzNjA1NX0.Kl9B9-RhPtW-32kgU61Gkqq4RxPHc3k8b8PFtQXHjj0',
+  //}]);
+//
   
-  supabaseAdmin.storage.from('bucket').getPublicUrl('13.png', {
-    transform: {
-      width: 500,
-      height: 600,
-    },
-  })
+  //supabaseAdmin.storage.from('bucket').getPublicUrl('13.png', {
+  //  transform: {
+  //    width: 500,
+  //    height: 600,
+  //  },
+  //})
   const { data } = await supabaseAdmin.from('images').select('*');
   return {
     props: {
